e2ed5eb9fb6c91924c51d57cae48159eb9c99ae8
Myles Braithwaite <me@mylesbraithwaite.com>
2016-08-02 11:24:56 -0400

    Fixed up a bunch of stuff.

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,12 @@
 import os
+import smtplib
+from email.mime.text import MIMEText
 
 from dateutil.rrule import rrule, MONTHLY, TU
 from dateutil.parser import parse as dateutil_parse
 
-from fabric.utils import abort
 from fabric.api import task, prompt
+from fabric.utils import abort, fastprint
 
 
 @task
@@ -12,8 +14,8 @@ def new_meeting():
     with open('_templates/meeting.txt.tpl', 'r') as f:
         meeting_template = f.read()
 
-    date_prompt = dateutil_parse(prompt('Date'))
-    title_prompt = prompt('Title')
+    date_prompt = dateutil_parse(prompt('Date:'))
+    title_prompt = prompt('Title:')
 
     date = list(rrule(MONTHLY, count=1, byweekday=TU, bysetpos=2,
                       dtstart=date_prompt))[0]
@@ -31,3 +33,34 @@ def new_meeting():
 
     with open(filepath, 'w') as f:
         f.write(meeting_template.format(**data))
+
+
+@task
+def send_meeting():
+    date = prompt('Date:', validate=r'(\d{4})-(\d{2})')
+    subject = prompt('Subject:')
+    email_from = 'hi@gtalug.org'
+    email_to = 'announce@gtalug.org'
+
+    filepath = 'meetings/{0}.txt'.format(date)
+
+    if not os.path.isfile(filepath):
+        return abort('There is no meeting at that date.')
+
+    with open(filepath, 'r') as f:
+        msg = MIMEText(f.read())
+
+    msg['Subject'] = subject
+    msg['From'] = email_from
+    msg['To'] = email_to
+
+    preview = "{0}".format(msg.as_string())
+
+    fastprint(preview)
+
+    send = prompt('Everything look okay?', validate=r'(yes|no)')
+
+    if send == 'yes':
+        s = smtplib.SMTP('penguin.gtalug.org')
+        s.sendmail(email_from, [email_to], msg.as_string())
+        s.quit()
